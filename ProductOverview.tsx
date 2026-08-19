211e3a3e8db23b74a8089ed3673c1edd5485f632
Samreen Malik <129145000+Samreen85@users.noreply.github.com>
2026-08-19 21:33:01 +0500

    Refactor ProductOverview component layout

--- ProductOverview.tsx
+++ ProductOverview.tsx
@@ -1,9 +1,19 @@
+import React from "react"
+
 export default function ProductOverview() {
   return (
     <div>
+      <img src="/hero.png" />
+
       <div onClick={handleSave}>Save</div>
-      <button><IconClose /></button>
+
+      <button>
+        <IconClose />
+      </button>
+
       <h1 style={{ fontSize: "72px", color: "#ff0000" }}>Settings</h1>
+
+      <input placeholder="Email" />
     </div>
   )
 }
